19aacbfdb9946d78086d4b161867dbb528e46107
René Föhring <rene@bamaru.de>
2010-08-10 00:27:03 +0200

    Companies in search reults (Sidebar)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,14 @@ class SearchController < ApplicationController
   login_required
   
   def index
-    @results = Search.find(params[:q])
-    @station_results = Search.find(params[:q], [Station])
+    q = params[:q]
+    @results = {
+      :main => Search.find(q)
+    }
+    @sidebar_result_models = %w(company station)
+    @sidebar_result_models.each do |klass|
+      @results[klass] = Search.find(q, [klass.classify])
+    end
   end
   
 end
