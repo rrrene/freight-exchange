31950183ef4d1d0aa1d36672c6e5b91c1ca3b970
René Föhring <rene@bamaru.de>
2010-08-11 22:38:04 +0200

    Added jQuery; added auto-fill user_login in company#new

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,18 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function login_from_name(name) {
+  var login = name.toLowerCase();
+  login = login.replace(/ä/g, 'ae');
+  login = login.replace(/ö/g, 'oe');
+  login = login.replace(/ü/g, 'ue');
+  login = login.replace(/(à|À|á|Á|â|Â|ã|ä|Ä|å|æ)/g, 'a');
+  login = login.replace(/(ç)/g, 'c');
+  login = login.replace(/(è|È|é|É|ê|Ê|ë)/g, 'e');
+  login = login.replace(/(ì|Ì|í|Í|î|Î|ï)/g, 'i');
+  login = login.replace(/(ñ)/g, 'n');
+  login = login.replace(/(ò|Ò|ó|Ó|õ|ö|Ö|ø|ô|Ô)/g, 'o');
+  login = login.replace(/(ù|Ù|ú|Ú|û|Û|ü|Ü)/g, 'u');
+  login = login.replace(/(ý|Ý|ÿ)/g, 'y');
+  login = login.replace(/[^A-Za-z0-9]+/g, '_');
+  return login;
+}
