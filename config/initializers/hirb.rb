443e271127e335ff99c5eb9869af820b8c5db9af
René Föhring <rene@bamaru.de>
2010-08-11 23:36:02 +0200

    Improved Hirb output in terminal

--- config/initializers/hirb.rb
+++ config/initializers/hirb.rb
@@ -1,1 +1,14 @@
 Hirb.enable
+
+fields = {
+  User => %w(id company_id login email),
+}
+
+output = {}
+fields.each do |model, fields|
+  output[model.to_s] = {
+    :options => {:fields => fields}
+  }
+end
+
+Hirb::View.enable :output => output
