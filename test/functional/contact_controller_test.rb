95fe730926f499ee563c13172cdc208e0e4c9263
René Föhring <rene@bamaru.de>
2011-02-27 10:45:44 +0100

    on tests: basic testing for contact_controller

--- test/functional/contact_controller_test.rb
+++ test/functional/contact_controller_test.rb
@@ -5,4 +5,10 @@ class ContactControllerTest < ActionController::TestCase
     assert_login_required_for :index
   end
 
+  test "should post contact" do
+    with_login do
+      post :index, :text => 'some contact request'
+      assert_response :redirect
+    end
+  end
 end
