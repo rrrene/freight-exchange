2670edf2d1f9ae9308c480af0acef08ba2ccdaa4
René Föhring <rf@bamaru.de>
2011-07-17 09:00:50 +0200

    worked on notifications ui

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,2 +1,6 @@
 module NotificationsHelper
+  
+  def relevant_notification_items_count
+    @relevant_notification_items_count ||= @unread ? @unread.count : current_user.unread_notification_items.count
+  end
 end
