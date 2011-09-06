496b6217e91b159990852b25b8548d8862dfc3f1
René Föhring <rf@bamaru.de>
2011-09-06 21:46:09 +0200

    little bugfixes and wording

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -6,9 +6,11 @@ class PeopleController < RemoteController
   role_or_ownership_required [:company_admin, :company_employee], :only => [:edit, :update]
   role_required [:company_admin, :company_employee], :only => [:index, :new, :create]
   
-  def index
-    index! {
-      @people = current_company.people
-    }
+  def filter_collection!
+    self.collection = current_company.people
+  end
+  
+  def order_map
+    super.merge(:name => "UPPER(last_name) ASC")
   end
 end
