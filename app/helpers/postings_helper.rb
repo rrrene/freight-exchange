103dcebe6606c9d6b270d5c11a9ccd9379a3a4d0
René Föhring <rf@bamaru.de>
2012-03-03 22:48:05 +0100

    Highlight postings of favored companies in a distinct way

--- app/helpers/postings_helper.rb
+++ app/helpers/postings_helper.rb
@@ -1,2 +1,18 @@
 module PostingsHelper
+
+  def posting_html_classes(ar)
+    classes = []
+    classes << if ar.try(:valid_until).to_i < Time.now.to_i
+      :old
+    else
+      :new
+    end
+    if @highlight && @highlight.include?(ar.id.to_s)
+      classes << "highlight"
+    end
+    if white_listed?(ar.company)
+      classes << "white_listed"
+    end
+    classes * " "
+  end
 end
