ac683fd81f6559bab332c68409b612d69cf8c580
René Föhring <rf@bamaru.de>
2012-03-04 19:04:30 +0100

    Style postings#show

--- app/helpers/postings_helper.rb
+++ app/helpers/postings_helper.rb
@@ -15,4 +15,21 @@ module PostingsHelper
     end
     classes * " "
   end
+
+  def posting_attribute(attribute, opts = {})
+    name = resource_class.human_attribute_name(attribute)
+    value = resource.send(attribute)
+    if opts[:humanize]
+      value = resource.human_attribute_value(attribute)
+    end
+    if opts[:localize] || value.is_a?(DateTime)
+      value = l(value)
+    end
+    if [true, false].include?(value)
+      value = value ? t("common.choice_yes") : t("common.choice_no")
+    end
+    if value
+      render :partial => "postings/show_attribute", :locals => {:name => name, :value => value, :dependent => !!opts[:dependent]}
+    end
+  end
 end
