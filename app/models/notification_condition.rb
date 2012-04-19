e27a4f5941f93fb6f789f4c90409b9f4a3312c17
René Föhring <rf@bamaru.de>
2012-04-19 08:22:23 +0200

    Add and localize :first_transport_at to notification_conditions

--- app/models/notification_condition.rb
+++ app/models/notification_condition.rb
@@ -1,7 +1,7 @@
 class NotificationCondition < ActiveRecord::Base
   belongs_to :notification_condition_set
 
-  ATTRIBUTE_NAME_CHOICES = %w(origin_city destination_city frequency)
+  ATTRIBUTE_NAME_CHOICES = %w(origin_city destination_city frequency first_transport_at)
   QUALIFIER_CHOICES = %w(equal)
 
   validates_presence_of :notification_condition_set
